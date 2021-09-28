646b533cc26738a87eac7b02d70e3cd4a7d26774
nedashkivskyios <nedashkivskyios@gmail.com>
2021-09-28 07:57:20 +0300

    Skill Component Add

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Skills.module.css'
+import {Skill} from "./Skill/Skill";
 
 export const Skills = () => {
   return (
     <div id={'skills'} className={styles.skills}>
-      Skills
+      <div className={styles.skillsTitle}>My skills</div>
+      <div className={styles.skillsItems}>
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
+
     </div>
   );
 };
